1eba035b8d774a9227ef56cf70844fa4a93456c8
Cleiton <cleitonrmacedo@gmail.com>
2023-02-20 16:54:14 -0300

    feat: Requisito 3 - Implementado a funcao process no modulo file_process. Essa funcao e capaz de transformar o conteudo da lista gerada pela funcao txt_importer em um dicionario que e armazenado dentro da Queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in instance._data: # lista inicial queue
+        if index["nome_do_arquivo"] == path_file:
+            return None
+
+    arquive = txt_importer(path_file)
+
+    dict_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(arquive),
+        "linhas_do_arquivo": arquive
+    }
+    instance.enqueue(dict_file)
+    print(dict_file, file=sys.stdout)
+    # Para garantir mostrar via stdout os dados processados
 
 
 def remove(instance):
