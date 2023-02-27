104952514c7b3d91b4a1118ff999f4cc4b4a4be7
Cleiton <cleitonrmacedo@gmail.com>
2023-02-27 03:33:21 -0300

    feat: Requisito 7 - Implementado funcao exists_word, dentro do modulo word_search, que verifica a existencia de uma palavra em todos os arquivos processados

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word: str, instance: Queue):
+    results = []
+    occurrences = []
+    
+    for index, line in enumerate(instance.search(0)["linhas_do_arquivo"]):
+        if word.lower() in line.lower():
+            occurrences.append({"linha": index + 1})
+
+    if occurrences:
+        results.append({
+            "palavra": word,
+            "arquivo": instance.search(0)["nome_do_arquivo"],
+            "ocorrencias": occurrences
+        })
+
+    return results
 
 
 def search_by_word(word, instance):
