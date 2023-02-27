424a22a40739f350b8bb342eafe0071033bf6824
Cleiton <cleitonrmacedo@gmail.com>
2023-02-27 03:39:21 -0300

    fix: lint - Linha 7: W293 blank line contains whitespace

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -4,7 +4,7 @@ from ting_file_management.queue import Queue
 def exists_word(word: str, instance: Queue):
     results = []
     occurrences = []
-    
+
     for index, line in enumerate(instance.search(0)["linhas_do_arquivo"]):
         if word.lower() in line.lower():
             occurrences.append({"linha": index + 1})
